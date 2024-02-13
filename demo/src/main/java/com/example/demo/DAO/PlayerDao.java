4bfafe25c99b36b74e46381b4baaac3dc96cdf62
JuliB-dotcom <69867011+JuliaB-dotcom@users.noreply.github.com>
2024-02-13 20:38:08 +0100

    Adding DAO and Controller classes

---
+++ demo/src/main/java/com/example/demo/DAO/PlayerDao.java
@@ -0,0 +1,6 @@
+package com.example.demo.DAO;
+// DAO Sends and retrieves data from storage, instead of queries.
+// Provides CRUD operations.
+public class PlayerDao {
+  
+}
---
+++ demo/src/main/java/com/example/demo/controllers/gameController.java
@@ -0,0 +1,5 @@
+package com.example.demo.controllers;
+// This class is the REST controller and handles incoming HTTP requests and defines the API endpoints.
+public class gameController {
+  
+}
